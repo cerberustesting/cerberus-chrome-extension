545e5d441ffd33eba2118bf6dd80ac887e166db6
MorganLmd <morgan.lombard@protonmail.com>
2021-06-29 18:19:04 +0200

    new xpath algorithm + notification after copy to clipboard + styling

--- background.js
+++ background.js
@@ -15,9 +15,7 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
       message.action = "erratum";
       message.isActive = newValue;
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
-          console.log(response.text);
-        });
+        chrome.tabs.sendMessage(tabs[0].id, message);
       });
     }
   }
--- contentScript.js
+++ contentScript.js
@@ -1,53 +1,90 @@
-console.log("Hello from content script");
-
-// TEmporary test object to save the result into the storage
+// Temporary test object to save the result into the storage
 const cerberusElement = {
   action: "",
   dateTime: "",
   content: "",
 };
 
-// function to get full xpath from the current page
-const getElementXpath = function (nodeElement) {
-  const elementIndex = (sibling, name) => {
-    return sibling
-      ? elementIndex(
-          sibling.previousElementSibling,
-          name || sibling.localName
-        ) +
-          (sibling.localName == name)
-      : 1;
-  };
-
-  const segments = (element) => {
-    if (!element || element.nodeType !== 1) {
-      return [""];
-    } else {
-      return [
-        ...segments(element.parentNode),
-        `${element.localName.toLowerCase()}[${elementIndex(element)}]`,
-      ];
+// function to generate xpath from https://github.com/trembacz/xpath-finder
+const getXPathFromElement = (element) => {
+  let nodeElement = element;
+  const parts = [];
+  while (nodeElement && nodeElement.nodeType === Node.ELEMENT_NODE) {
+    let nbOfPreviousSiblings = 0;
+    let hasNextSiblings = false;
+    let sibling = nodeElement.previousSibling;
+    while (sibling) {
+      if (
+        sibling.nodeType !== Node.DOCUMENT_TYPE_NODE &&
+        sibling.nodeName === nodeElement.nodeName
+      ) {
+        nbOfPreviousSiblings++;
+      }
+      sibling = sibling.previousSibling;
     }
-  };
-
-  return segments(nodeElement).join("/");
+    sibling = nodeElement.nextSibling;
+    while (sibling) {
+      if (sibling.nodeName === nodeElement.nodeName) {
+        hasNextSiblings = true;
+        break;
+      }
+      sibling = sibling.nextSibling;
+    }
+    const prefix = nodeElement.prefix ? `${nodeElement.prefix}:` : "";
+    const nth =
+      nbOfPreviousSiblings || hasNextSiblings
+        ? `[${nbOfPreviousSiblings + 1}]`
+        : "";
+    parts.push(prefix + nodeElement.localName + nth);
+    nodeElement = nodeElement.parentNode;
+  }
+  return parts.length ? `/${parts.reverse().join("/")}` : "";
 };
 
 // function to get full source code <html>...</html> from the current page
-const getPageSourceCode = function () {
+const getPageSourceCode = () => {
   return document.documentElement.outerHTML;
 };
 
+const displayCopyNotification = () => {
+  const notificationContainerHtml = `<div id="notification-container"></div>`;
+  const notificationId = `${cerberusElement.action}-${cerberusElement.dateTime}`;
+  const notificationHTML = `
+    <div id="${notificationId}" class="notification">
+      <p>${cerberusElement.content.slice(0, 40)}...</p>
+    </div>
+  `;
+
+  let notificationContainer = document.getElementById("notification-container");
+
+  if (!notificationContainer) {
+    console.log("inject notif container");
+    document.body.insertAdjacentHTML("beforeend", notificationContainerHtml);
+    notificationContainer = document.getElementById("notification-container");
+  }
+  notificationContainer.insertAdjacentHTML("afterbegin", notificationHTML);
+
+  setTimeout(() => {
+    const notification = document.getElementById(notificationId);
+    if (notification) {
+      notificationContainer.removeChild(notification);
+    }
+  }, 3000);
+};
+
 // function to save element to the clipboard by temporary creating a new node
-const copyToClipboard = function (elementToCopy) {
+const copyToClipboard = () => {
   let temporaryElement = document.createElement("textarea");
   document.body.appendChild(temporaryElement);
-  temporaryElement.value = elementToCopy;
+  temporaryElement.value = cerberusElement.content;
   temporaryElement.select();
   document.execCommand("copy");
   document.body.removeChild(temporaryElement);
+  displayCopyNotification();
 };
 
+/*
+// TODO FOR NEXT IMPLEMENTATION
 // function to save an element to the sync storage with a cerberusElement object
 const saveToStorage = function (action, elementToSave) {
   cerberusElement.action = action;
@@ -63,57 +100,62 @@ const saveToStorage = function (action, elementToSave) {
     chrome.storage.sync.set(cerberusElements);
   });
 };
+*/
 
 // main erratum function
-const getErratumElement = function (event) {
-  const xpath = getElementXpath(event.target);
+const getErratumElement = (event) => {
+  event.preventDefault();
+  const xpath = getXPathFromElement(event.target);
   const htmlSourceCode = getPageSourceCode();
-  const erratumElement = `erratum=${xpath},${htmlSourceCode}`;
-  copyToClipboard(erratumElement);
-  saveToStorage("erratum", erratumElement);
-  console.log(xpath);
-  console.log(erratumElement);
+  cerberusElement.action = "erratum";
+  cerberusElement.dateTime = new Date().toLocaleString();
+  cerberusElement.content = `erratum=${xpath},${htmlSourceCode}`;
+  console.log(cerberusElement);
+  copyToClipboard();
+  //saveToStorage("erratum", erratumElement);
 };
 
 // handle node highlitghting on page
-const addHighlightOnElement = function (event) {
+const addHighlightOnElement = (event) => {
+  event.preventDefault();
   // highlight the mouseover target
-  event.target.style.backgroundColor = "orange";
-  event.target.parentNode.backgroundColor = "blue";
+  event.target.style.backgroundColor = "#333333";
+  event.target.style.color = "white";
+  event.target.style.cursor = "pointer";
 };
 
-const removeHighlightOnElement = function (event) {
-  // highlight the mouseover target
+const removeHighlightOnElement = (event) => {
+  event.preventDefault();
+  // remove highlight  the mouseover target
   event.target.style.backgroundColor = "";
-  event.target.parentNode.backgroundColor = "";
+  event.target.style.color = "";
+  event.target.style.cursor = "default";
 };
 
-// functions to add or remove listeners
-const setUpErratum = function () {
+// functions to add or remove listeners and activate action
+const setUpAction = (eventType, action) => {
   document.addEventListener("mouseover", addHighlightOnElement, false);
   document.addEventListener("mouseout", removeHighlightOnElement, false);
-  document.addEventListener("click", getErratumElement);
+  document.addEventListener(eventType, action);
 };
 
-const removeErratum = function () {
+const removeAction = (eventType, action) => {
   document.removeEventListener("mouseover", addHighlightOnElement, false);
   document.removeEventListener("mouseout", removeHighlightOnElement, false);
-  document.removeEventListener("click", getErratumElement);
+  document.removeEventListener(eventType, action);
+};
+
+const toggleErratumAction = (request) => {
+  request.action === "erratum" && request.isActive === true
+    ? setUpAction("click", getErratumElement)
+    : removeAction("click", getErratumElement);
 };
 
 // function that receive a message from the background service worker with the action to perform
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(request);
-  console.log(
-    sender.tab
-      ? "from a content script:" + sender.tab.url
-      : "received message from the backgound service"
-  );
-  if (request.action === "erratum" && request.isActive === true) {
-    setUpErratum();
-    sendResponse({ text: "erratum is active" });
+  if (request.action === "erratum") {
+    toggleErratumAction(request);
   } else {
-    removeErratum();
-    sendResponse({ text: "erratum is not active" });
+    console.err("unsupported function");
   }
 });
